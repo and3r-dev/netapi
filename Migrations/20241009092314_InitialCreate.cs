dd7cf1a5b6cb16600daec599245272afcb64afa5
anderson.silva <neo.and3r@gmail.com>
2024-10-11 18:22:08 -0300

    migrations, controllers, models

---
+++ Migrations/20241009092314_InitialCreate.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VeiculoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Placa = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Ano = table.Column<int>(type: "int", nullable: false),
+                    Cor = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Modelo = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Caminhoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CapacidadeCarga = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    VeiculoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Caminhoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Caminhoes_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Carros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CapacidadePassageiro = table.Column<int>(type: "int", nullable: false),
+                    VeiculoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Carros_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Revisoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Km = table.Column<int>(type: "int", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ValorDaRevisao = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    VeiculoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Revisoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Revisoes_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Veiculos",
+                columns: new[] { "Id", "Ano", "Cor", "Modelo", "Placa" },
+                values: new object[,]
+                {
+                    { 1, 2020, "Preto", "Sedan", "ABC1234" },
+                    { 2, 2018, "Branco", "Caminhão", "XYZ5678" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Caminhoes",
+                columns: new[] { "Id", "CapacidadeCarga", "VeiculoId" },
+                values: new object[] { 1, 15000m, 2 });
+
+            migrationBuilder.InsertData(
+                table: "Carros",
+                columns: new[] { "Id", "CapacidadePassageiro", "VeiculoId" },
+                values: new object[] { 1, 5, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Revisoes",
+                columns: new[] { "Id", "Data", "Km", "ValorDaRevisao", "VeiculoId" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 10000, 500m, 1 },
+                    { 2, new DateTime(2022, 6, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), 20000, 800m, 1 },
+                    { 3, new DateTime(2023, 3, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), 50000, 1200m, 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Caminhoes_VeiculoId",
+                table: "Caminhoes",
+                column: "VeiculoId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carros_VeiculoId",
+                table: "Carros",
+                column: "VeiculoId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Revisoes_VeiculoId",
+                table: "Revisoes",
+                column: "VeiculoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Caminhoes");
+
+            migrationBuilder.DropTable(
+                name: "Carros");
+
+            migrationBuilder.DropTable(
+                name: "Revisoes");
+
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
